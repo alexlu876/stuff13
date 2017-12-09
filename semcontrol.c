5f7f3453710a439e4f6b1b4bfabc49f4c4fab44e
JFreud <jrm.frdnbrg@gmail.com>
2017-12-09 14:08:12 -0500

    worked on control

--- semcontrol.c
+++ semcontrol.c
@@ -13,34 +13,77 @@
 #define KEY 174209
 
 
-int main(int argc, char* argv[]){
-  int sd;
-  int i = 0;
-  while(argv[i]){
-  if(!(strcmp(argv[i], "-c"))){
-    sd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-    if(sd == -1){
-      printf("Silly you, a semaphore already exists!\n");
-      return 0;
-    }
-    else{
-      printf("Semaphore created. Key: %d\n", KEY);
-      semctl(sd, 0, SETVAL, atoi(argv[i+1]));
-    }
+char * get_story() {
+  int fd = open("story.txt", O_RDONLY);
+  if (fd == -1) {
+    printf("No story found. Create using -c\n");
+    exit(1);
   }
-  if(!(strcmp(argv[i], "-v"))){
-    printf("Semaphore value: %d\n", semctl(semget(KEY, 0, 0), 0, GETVAL));
+  int size = lseek(fd, 0, SEEK_END);
+  if (size == -1) {
+    printf("%s\n", strerror(errno));
   }
-  if(!(strcmp(argv[i], "-r"))){
-    semctl(semget(KEY, 0, 0), 0, IPC_RMID);
-    printf("Semaphore removed\n");
-  }
-  i++;
-  }
+  char * text = malloc(size);
+  read (fd, text, size);
+  return text;
 }
 
 
 
+int main(int argc, char* argv[]){
+  int semid, fd, shmid;
+  union semun val;
+  void *shm;
 
 
+  int i = 0;
+  while(argv[i]){
+  if(!(strcmp(argv[i], "-c"))){
+    semid = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0600); //create semaphore
+    if(semid == -1){
+      printf("Silly you, a semaphore already exists!\n");
+      return 0;
+    }
+    else{
+      val.val = 1;
+      printf("Semaphore created. Key: %d\t Value: %d\n", KEY, semctl(semid, 0, SETVAL, val)); //set semaphore value
+      shmid = shmget(KEY, sizeof(int), IPC_CREAT | 0666); //create shared memory
+      if (shmid == -1) { // if it failed
+        printf("Bad shared memory: %s\n", strerror(errno));
+        exit(1);
+      }
+      shm = shmat(shmid, 0, 0); //shared memory pointer
+      fd = creat("story.txt", 0644); //create file
+    }
+  }
+  if(!(strcmp(argv[i], "-v"))){
+    char * text = get_story();
+    printf("Entire story:\n%s\n", text);
+    free(text);
+  }
+  if(!(strcmp(argv[i], "-r"))){
+    semid = semget(KEY, 1, 0);
+    int semcheck = semctl(semid, 0, IPC_RMID); //remove semaphore
+    if (semcheck == -1) {
+      printf("Couldn't access semaphore: %s\n", strerror(errno));
+      exit(1);
+    }
+    printf("Semaphore removed\n");
 
+    shmid = shmget(KEY, sizeof(int), 0);
+    int shmcheck = shmctl(shmid, IPC_RMID, 0); //remove shared memory
+    if (shmcheck == -1) {
+      printf("Couldn't remove shared memory: %s\n", strerror(errno));
+      exit(1);
+    }
+    printf("Shared memory removed\n");
+    char * text = get_story();
+    printf("Entire story:\n%s\n", text);
+    free(text);
+    remove("story.txt"); //remove story
+  }
+  i++;
+
+  }
+  return 0;
+}
---
+++ story.c
@@ -0,0 +1,25 @@
+#include "semcontrol.c"
+
+
+int main(int argc, char* argv[]) {
+
+  int fd;
+
+  if(!(strcmp(argv[i], "-v"))){
+    fd = open("story.txt", O_RDONLY);
+    
+
+  }
+
+
+  control(argc, argv);
+
+
+
+  sd = semget(KEY, 1, 0600);
+
+  line[10000];
+  read(fd, &line, sizeof(line));
+  printf("Last line: ")
+
+}
